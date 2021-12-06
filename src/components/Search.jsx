f94da666127d2fbe60f8890f6269fa9df0abb151
thomas <thomas.chalanson@sfr.fr>
2021-12-06 18:28:27 +0100

    continue fetching the api

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,20 +1,30 @@
 import React, { useState, useEffect } from "react";
+import Temps from "./Temps";
 
 export default function Search() {
-  const [cities, setCities] = useState("");
+  const [cities, setCities] = useState('Lyon');
+  const [country, setCountry] = useState(undefined);
   const [weather, setWeather] = useState([]);
+  const [tempMax, setTempMax] = useState(null);
+  const [tempMin, setTempMin] = useState(null);
+
+  const Api_key = "98b7465353d383f3d0f3bc4a284a48ae";
 
   useEffect(() => {
     fetch(
-      `http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=98b7465353d383f3d0f3bc4a284a48ae`
+      `http://api.openweathermap.org/data/2.5/weather?q=${cities},${country}&appid=${Api_key}`
     )
       .then((res) => res.json())
       .then((data) => {
-        setWeather(data);
+        setWeather(data.city);
         setCities("");
         console.log("api", data);
+        // setTempMax(data.main.temp_max);
+        // console.log("max", data.main.temp_max);
       });
   }, []);
+
+  const handleChange = (e) => setCities(e.target.value);
 
   return (
     <>
@@ -25,7 +35,13 @@ export default function Search() {
           id="searchBar"
           className="search-bar"
           placeholder="Lyon"
+          value={cities}
+          onChange={handleChange}
         />
+        <div>{cities}</div>
+        <div>
+          <Temps city={cities} />
+        </div>
       </div>
     </>
   );
--- src/components/Temps.jsx
+++ src/components/Temps.jsx
@@ -1,16 +1,18 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React, { useState } from "react";
+import PropTypes from "prop-types";
 
-const Temps = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+const Temps = ({ name, temps_max, temps_min }) => {
+  const [cold, setCold] = useState("Il fait froid");
+  const [warm, setWarm] = useState("Il fait chaud");
+
+  return (
+    <div>
+      <div className="city-name">{name}</div>
+      <div className="temps">{(temps_max, temps_min)}</div>
+    </div>
+  );
 };
 
-Temps.propTypes = {
-    
-};
+Temps.propTypes = {};
 
 export default Temps;
