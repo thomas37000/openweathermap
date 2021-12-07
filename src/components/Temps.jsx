a526fa8ae38e5d77eee0cafd9f5962664d5493c7
thomas <thomas.chalanson@sfr.fr>
2021-12-07 15:27:17 +0100

    fetch the warm json list

--- src/components/Temps.jsx
+++ src/components/Temps.jsx
@@ -1,12 +1,15 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import PropTypes from "prop-types";
 import { fetchWeather } from "../api/api";
+import axios from "axios";
+import "./Temps.css";
 
 const Temps = ({ name, temps_max, temps_min }) => {
   const [cold] = useState("Il fait froid");
   const [warm] = useState("Il fait chaud");
   const [cities, setCities] = useState("");
   const [weather, setWeather] = useState({});
+  const [warmCities, setWarmCities] = useState([]);
 
   const search = async (e) => {
     if (e.key === "Enter") {
@@ -16,7 +19,34 @@ const Temps = ({ name, temps_max, temps_min }) => {
       console.log("api", res);
     }
   };
+
   const handleChange = (e) => setCities(e.target.value);
+
+  useEffect(() => {
+    const loadJsonWarmList = async () => {
+      setTimeout(async () => {
+        const res = await axios.get(
+          "https://raw.githubusercontent.com/thomas37000/openweathermap/main/src/components/WarmList.json"
+        );
+        setWarmCities(res.data);
+        console.log("warm", res.data);
+      }, 500);
+    };
+    loadJsonWarmList();
+  }, []);
+
+  const fetchJsonWarmList =
+    warmCities &&
+    warmCities.map((list, i) => {
+      return (
+        <>
+          <div className="warm-list">
+            <div>{list.city}</div>
+            {/* <div>{list.country}</div> */}
+          </div>
+        </>
+      );
+    });
 
   return (
     <div className="search">
@@ -47,6 +77,12 @@ const Temps = ({ name, temps_max, temps_min }) => {
           <div>{weather.main.temp < 15 ? cold : warm}</div>
         </>
       )}
+
+      <div className="vacation">
+        Do you want to go in vacanation in a warm City, here some examples where
+        the temperature is superior at 25°
+        <div>{fetchJsonWarmList}</div>
+      </div>
     </div>
   );
 };
