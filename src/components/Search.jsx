6e68287e90a0903bd39bd82d29854366f969e642
thomas <thomas.chalanson@sfr.fr>
2021-12-07 12:29:06 +0100

    destructure the api load

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,38 +1,25 @@
 import axios from "axios";
 import React, { useState, useEffect } from "react";
+import { fetchWeather } from "../api/api";
 import Temps from "./Temps";
 
 export default function Search() {
   const [cities, setCities] = useState("Lyon");
   const [country, setCountry] = useState(undefined);
-  const [weather, setWeather] = useState([]);
+  const [weather, setWeather] = useState({});
   const [tempMax, setTempMax] = useState(null);
   const [tempMin, setTempMin] = useState(null);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
 
-  const Api_key = "98b7465353d383f3d0f3bc4a284a48ae";
-
-  useEffect(() => {
-    const loadApi = async () => {
-      setTimeout(async () => {
-        try {
-          const res = await axios.get(
-            `http://api.openweathermap.org/data/2.5/weather?q=${cities},${country}&appid=${Api_key}`
-          );
-          setWeather(res.data);
-          console.log("api", res.data);
-          // console.log("test", res.data.cities);
-        } catch (error) {
-          setError(error);
-        } finally {
-          setLoading(false);
-        }
-      }, 500);
-    };
-
-    loadApi();
-  }, []);
+  const search = async (e) => {
+    if (e.key === "Enter") {
+      const res = await fetchWeather(cities);
+      setWeather(res);
+      setCities("");
+      console.log("api", res);
+    }
+  };
 
   const handleChange = (e) => setCities(e.target.value);
 
--- src/components/Temps.jsx
+++ src/components/Temps.jsx
@@ -1,14 +1,28 @@
 import React, { useState } from "react";
 import PropTypes from "prop-types";
 
-const Temps = ({ name, temps_max, temps_min }) => {
+const Temps = ({ name, main, sys, temps_max, temps_min, weather }) => {
   const [cold, setCold] = useState("Il fait froid");
   const [warm, setWarm] = useState("Il fait chaud");
 
   return (
-    <div>
-      <div className="city-name">{name}</div>
-      <div className="temps">{(temps_max, temps_min)}</div>
+    <div className="city">
+      <h2 className="city-name">
+        <span>{name}</span>
+        {/* <sup>{sys.country}</sup> */}
+      </h2>
+      {/* <div className="city-temp">
+        {Math.round(main.temp)}
+        <sup>&deg;C</sup>
+      </div> */}
+      {/* <div className="info">
+        <img
+          className="city-icon"
+          src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
+          alt={weather.weather[0].description}
+        /> 
+        <p>{weather[0].description}</p>
+      </div> */}
     </div>
   );
 };
